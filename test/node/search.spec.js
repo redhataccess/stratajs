c2786d61379dab4c80930c991e282642d9c61cc7
Connor Hindley <conn.hindley@gmail.com>
2014-11-26 16:08:31 -0500

    more search tests, bump timeout

--- test/node/search.spec.js
+++ test/node/search.spec.js
@@ -3,11 +3,13 @@
 var strata = require('../../strata');
 var expect = require('chai').expect;
 
+var ARTICLE_TITLE = 'Bash Code Injection Vulnerability via Specially Crafted Environment Variables (CVE-2014-6271, CVE-2014-7169)',
+    SOLUTION_TITLE = 'How to troubleshoot kernel crashes, hangs, or reboots with kdump on Red Hat Enterprise Linux';
 
 function noop() {}
 describe('Search', function () {
     // groan.
-    this.timeout(10000);
+    this.timeout(15000);
 
     it('Should return 50 solutions/articles', function (done) {
         strata.search('rhel', function (data) {
@@ -42,7 +44,7 @@ describe('Search', function () {
     it('Should return one article by id', function (done) {
         strata.articles.get('1200223', function (data) {
             expect(data).to.be.ok();
-            expect(data.title).to.equal('Bash Code Injection Vulnerability via Specially Crafted Environment Variables (CVE-2014-6271, CVE-2014-7169)');
+            expect(data.title).to.equal(ARTICLE_TITLE);
             done();
         }, noop);
     });
@@ -50,7 +52,23 @@ describe('Search', function () {
     it('Should return one article by url', function (done) {
         strata.articles.get('https://api.access.redhat.com/rs/articles/1200223', function (data) {
             expect(data).to.be.ok();
-            expect(data.title).to.equal('Bash Code Injection Vulnerability via Specially Crafted Environment Variables (CVE-2014-6271, CVE-2014-7169)');
+            expect(data.title).to.equal(ARTICLE_TITLE);
+            done();
+        }, noop);
+    });
+
+    it('Should return one solution by id', function (done) {
+        strata.solutions.get('6038', function (data) {
+            expect(data).to.be.ok();
+            expect(data.title).to.equal(SOLUTION_TITLE);
+            done();
+        }, noop);
+    });
+
+    it('Should return one solution by url', function (done) {
+        strata.solutions.get('https://api.access.redhat.com/rs/solutions/6038', function (data) {
+            expect(data).to.be.ok();
+            expect(data.title).to.equal(SOLUTION_TITLE);
             done();
         }, noop);
     });
