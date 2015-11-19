2bbe781ba6481fd04726b16c7711892236e4efe3
Vikas Rathee <vrathee@dhcp193-87.pnq.redhat.com>
2015-11-19 13:21:07 +0530

    Merging Keyur's changes to have a single tag

--- strata.js
+++ strata.js
@@ -81,6 +81,7 @@
         fetchChatTranscript,
         createEscalation,
         caseReviewSelector,
+        solutionReviewSelector,
         attachmentMaxSize;
 
     strata.version = '1.4.2';
@@ -2075,6 +2076,30 @@
             }
         });
         $.ajax(caseReviewSelector);
+    };
+
+    strata.reviews.getSolutionNumber = function ( query, onSuccess, onFailure) {
+        if (!$.isFunction(onSuccess)) { throw 'onSuccess callback must be a function'; }
+        if (!$.isFunction(onFailure)) { throw 'onFailure callback must be a function'; }
+
+        var url = strataHostname.clone().setPath('/rs/recommendations?'+query);
+        solutionReviewSelector = $.extend({}, baseAjaxParams, {
+            url: url,
+            headers: {
+                accept: 'application/vnd.redhat.solr+json'
+            },
+            success: function (response) {
+                if (response !== undefined) {
+                    onSuccess(response);
+                } else {
+                    onSuccess([]);
+                }
+            },
+            error: function (xhr, reponse, status) {
+                onFailure('Error ' + xhr.status + ' ' + xhr.statusText, xhr, reponse, status);
+            }
+        });
+        $.ajax(reviewSelector);
     };
 
     return strata;
