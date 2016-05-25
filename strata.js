92c9f0e7d24d003a3f5c44fc4276cb83bc6f07fa
Vikas Rathee <vrathee@dhcp193-87.pnq.redhat.com>
2016-05-25 11:45:54 +0530

    Fix for PCM-2781

--- strata.js
+++ strata.js
@@ -440,7 +440,7 @@
             .addQueryParam('rows', String(limit))
             .addQueryParam('start', String(start)) // jsUri fubar's 0, it doesn't add it as a number, must force it to a string to be safe
             .addQueryParam('fl', '*,score')
-            .addQueryParam('fq', 'documentKind:(Solution)');
+            .addQueryParam('fq', 'documentKind:Solution');
 
         if (highlight) {
             url.addQueryParam('hl', 'true')
