7297ec6f660c70977a4fb88892eefe7a7ef2b380
Vikas Rathee <vrathee@dhcp193-87.pnq.redhat.com>
2016-09-21 15:36:31 +0530

    PCM-3222 removing ;

--- strata.js
+++ strata.js
@@ -441,7 +441,7 @@
             .addQueryParam('rows', String(limit))
             .addQueryParam('start', String(start)) // jsUri fubar's 0, it doesn't add it as a number, must force it to a string to be safe
             .addQueryParam('fl', '*,score')
-            .addQueryParam('fq', 'documentKind:Solution');
+            .addQueryParam('fq', 'documentKind:Solution')
             .addQueryParam('fq', '-internalTags:helper_solution');
 
         if (highlight) {
